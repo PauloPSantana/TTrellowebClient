3deb46a01ec5ab8837e1682c28b90186cba3dbf7
PauloPSantana <prog.santana@gmail.com>
2022-02-15 16:02:09 -0300

    Signed-off-by: PauloPSantana <prog.santana@gmail.com>

--- src/main/java/br/com/trello/conf/confTrello.java
+++ src/main/java/br/com/trello/conf/confTrello.java
@@ -1,7 +1,7 @@
 package br.com.trello.conf;
 
 public interface confTrello {
-	final static String Token ="720c461c5cb554a2fe50d625296804963dc4c8e04d4cac8b9e2df8b88ae752dd";
-	final static String key="c998ce34c5e8f2f7daceaf8c1bd9b434";
+	final static String Token ="Token";
+	final static String key="key";
 
 }
